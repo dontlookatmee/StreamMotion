3fb2c415eb1e040736de6f6878301f75fbd7ef5e
Daniel <darknessbg@windowslive.com>
2020-07-02 13:06:42 +0300

    register component cleanup

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -8,11 +11,47 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup = this.fb.group({
-    email: [''],
-    password: [''],
+    email: ['', [Validators.required, Validators.pattern(/\S+@\S+\.\S+/)]],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(6), Validators.maxLength(30)],
+    ],
   });
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private fAuth: AngularFireAuth,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {}
+
+  handleEmailSingin() {
+    if (this.registerForm.valid) {
+      const data = {
+        email: this.registerForm.controls.email.value,
+        password: this.registerForm.controls.password.value,
+      };
+
+      this.fAuth
+        .signInWithEmailAndPassword(data.email, data.password)
+        .then((x) => {
+          this.router.navigate(['./']);
+        })
+        .catch((err) => {
+          alert(err);
+        });
+    }
+  }
+
+  handleGoogleSingin() {
+    this.fAuth
+      .signInWithPopup(new auth.GoogleAuthProvider())
+      .then((x) => {
+        this.router.navigate(['./']);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
 }
