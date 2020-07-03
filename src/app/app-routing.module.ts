982f36e2a7826e7124da87598158219f05b8b9d8
Daniel <darknessbg@windowslive.com>
2020-07-03 13:33:56 +0300

    global styles added, app comp container added, lazy load streams

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,12 @@ const routes: Routes = [
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'streams',
+    loadChildren: () =>
+      import('./streams/streams.module').then((m) => m.StreamsModule),
+    canActivate: [AuthGuard],
   },
 ];
 
