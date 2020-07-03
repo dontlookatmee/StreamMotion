3f40863b84e5a1de20dabadcd3d6b92170e31218
Daniel <darknessbg@windowslive.com>
2020-07-03 19:20:09 +0300

    minor css changes, added games comp as child route to streamers module

--- src/app/streams/streams-routing.module.ts
+++ src/app/streams/streams-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StreamsHomeComponent } from './streams-home/streams-home.component';
+import { GamesComponent } from './games/games.component';
 
 const routes: Routes = [
   {
     path: '',
     component: StreamsHomeComponent,
+    children: [
+      {
+        path: '',
+        component: GamesComponent,
+      },
+    ],
   },
 ];
 
--- src/app/streams/streams.module.ts
+++ src/app/streams/streams.module.ts
@@ -5,10 +5,11 @@ import { StreamsRoutingModule } from './streams-routing.module';
 import { StreamsHomeComponent } from './streams-home/streams-home.component';
 import { SidePanelComponent } from './side-panel/side-panel.component';
 import { StreamerMiniComponent } from './streamer-mini/streamer-mini.component';
+import { GamesComponent } from './games/games.component';
 
 
 @NgModule({
-  declarations: [StreamsHomeComponent, SidePanelComponent, StreamerMiniComponent],
+  declarations: [StreamsHomeComponent, SidePanelComponent, StreamerMiniComponent, GamesComponent],
   imports: [
     CommonModule,
     StreamsRoutingModule
