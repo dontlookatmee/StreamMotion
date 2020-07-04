db6ce483027686c16ed4550874a2c66bd1b575c7
Daniel <darknessbg@windowslive.com>
2020-07-04 19:07:00 +0300

    moved interfaces to models file

---
+++ src/app/models/models.ts
@@ -0,0 +1,18 @@
+export interface Token {
+  access_token: string;
+  expires_in: number;
+  token_type: string;
+}
+
+export interface Stream {
+  game_id: string;
+  id: string;
+  lagunage: string;
+  started_at: string;
+  thumbnail_url: string;
+  title: string;
+  type: string;
+  user_id: string;
+  user_name: string;
+  view_count: number;
+}
