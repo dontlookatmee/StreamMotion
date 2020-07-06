15df10ddf4da6b8defe5424343bb713c23c49ab6
Daniel <darknessbg@windowslive.com>
2020-07-06 13:03:15 +0300

    successfully fetch top games

--- src/app/streams/game-categories/game-categories.component.ts
+++ src/app/streams/game-categories/game-categories.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { TwitchService } from 'src/app/services/twitch/twitch.service';
+import { Games } from '../../models/models';
 
 @Component({
   selector: 'app-game-categories',
   templateUrl: './game-categories.component.html',
-  styleUrls: ['./game-categories.component.css']
+  styleUrls: ['./game-categories.component.css'],
 })
 export class GameCategoriesComponent implements OnInit {
+  topGames: Games;
 
-  constructor() { }
+  constructor(private tw: TwitchService) {}
 
   ngOnInit(): void {
+    this.tw.fetchTopGames().subscribe((games: Games) => {
+      this.topGames = games;
+    });
   }
-
 }
--- src/app/streams/streams.module.ts
+++ src/app/streams/streams.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { StreamsRoutingModule } from './streams-routing.module';
 import { StreamsHomeComponent } from './streams-home/streams-home.component';
 import { SidePanelComponent } from './side-panel/side-panel.component';
-import { GameCardComponent } from './game-card/game-card.component';
+import { GameCardComponent } from './game-categories/game-card/game-card.component';
 import { StreamerMiniComponent } from './side-panel/streamer-mini/streamer-mini.component';
 import { GameCategoriesComponent } from './game-categories/game-categories.component';
 import { StreamsAllComponent } from './streams-all/streams-all.component';
