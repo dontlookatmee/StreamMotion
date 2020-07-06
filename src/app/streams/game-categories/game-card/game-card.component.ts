fcfbdb4c90362b1382f5aec265caf74a69fc2798
Daniel <darknessbg@windowslive.com>
2020-07-06 13:12:24 +0300

    game card change detection is onpush now

--- src/app/streams/game-categories/game-card/game-card.component.ts
+++ src/app/streams/game-categories/game-card/game-card.component.ts
@@ -1,26 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-game-card',
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GameCardComponent implements OnInit {
   @Input('gameName') gameName: string;
   @Input('gameImage') gameImage: string;
   @Input('gameId') gameId: string;
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
+  redirectToGame() {
+    this.router.navigate(['./', this.gameId]);
+  }
+
   renderImage(image: string) {
-    console.log(this.gameImage);
     const endIndex = image.indexOf('{width}x{height}.jpg');
     if (endIndex !== -1) {
       return `${image.substr(0, endIndex)}100x100.jpg`;
     }
-
     return image;
   }
 }
